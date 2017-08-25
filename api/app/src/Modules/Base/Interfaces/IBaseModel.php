313ea0a837daf10f2bb46542a2544b13d960c0e0
Sheryl van Heerden <benshez@gmail.com>
2017-08-26 07:04:35 +1000

    Testing

--- api/app/src/Modules/Base/Interfaces/IBaseModel.php
+++ api/app/src/Modules/Base/Interfaces/IBaseModel.php
@@ -6,12 +6,12 @@ use Interop\Container\ContainerInterface;
 
 interface IBaseModel
 {
-	public function __construct(ContainerInterface $container);
-	public function get($sender, array $args);
-	public function setContainer(ContainerInterface $container);
-	public function getContainer();
-	public function setEntityManager();
-	public function getEntityManager();
-	public function getConfig();
-	public function getSettings();
+    public function __construct(ContainerInterface $container);
+    public function get($sender, array $args);
+    public function setContainer(ContainerInterface $container);
+    public function getContainer();
+    public function setEntityManager();
+    public function getEntityManager();
+    public function getConfig();
+    public function getSettings();
 }
