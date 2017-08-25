4c4b2f5c7c2a7063fca8566ad1cd22e06a562a80
Sheryl van Heerden <benshez@gmail.com>
2017-08-26 06:47:54 +1000

    Formatter

--- api/app/src/Modules/Base/Interfaces/IBaseValidation.php
+++ api/app/src/Modules/Base/Interfaces/IBaseValidation.php
@@ -5,15 +5,16 @@ namespace GeoService\Modules\Base\Interfaces;
 use Zend\Validator\ValidatorChain;
 use GeoService\Modules\Base\Interfaces\IBaseModel;
 
-interface IBaseValidation {
-	public function __construct(IBaseModel $model);
-	public function setModel(IBaseModel $model);
-	public function getModel();
-	public function setMessagesAray($error = null);
-	public function getMessagesAray();
-	public function isValid($value);
-	public function formIsValid(array $fields, array $values);
-	public function create();
-	public function createValidators(array $fields, array $values);
-	public function dispose();
+interface IBaseValidation
+{
+    public function __construct(IBaseModel $model);
+    public function setModel(IBaseModel $model);
+    public function getModel();
+    public function setMessagesAray($error = null);
+    public function getMessagesAray();
+    public function isValid($value);
+    public function formIsValid(array $fields, array $values);
+    public function create();
+    public function createValidators(array $fields, array $values);
+    public function dispose();
 }
