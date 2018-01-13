268f677359c04800d46baaab05b4924d5c3bd021
Ben van Heerden <benshez@gmail.com>
2018-01-13 11:31:43 +1000

    chore(): Added React

--- frontend/geoservice/src/components/menu/index.js
+++
@@ -1,3 +0,0 @@
-export * from './Menu';
-export * from './MenuButton';
-export * from './MenuContainer';
---
+++ frontend/index.web.js
@@ -0,0 +1,37 @@
+// @flow
+import 'babel-polyfill';
+import React from 'react';
+
+import { AppRegistry } from 'react-native';
+
+import Navigation from './src/navigation';
+
+import { AppContainer } from 'react-hot-loader';
+
+const renderApp = () => (
+  <AppContainer>
+    <Navigation />
+  </AppContainer>
+);
+
+AppRegistry.registerComponent('ReactNativeWebBoilerplate', () => renderApp);
+
+if (module.hot) {
+  // $FlowFixMe
+  module.hot.accept();
+
+  const renderHotApp = () => (
+    <AppContainer>
+      <Navigation />
+    </AppContainer>
+  );
+
+  AppRegistry.registerComponent(
+    'ReactNativeWebBoilerplate',
+    () => renderHotApp
+  );
+}
+
+AppRegistry.runApplication('ReactNativeWebBoilerplate', {
+  rootTag: document.getElementById('root')
+});
