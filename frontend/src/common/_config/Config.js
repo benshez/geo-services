deccfe3db4bcf88adcb9808d5b4c70778460c50a
Ben van Heerden <benshez@gmail.com>
2018-01-15 06:19:32 +1000

    chore: Remove Config

--- frontend/src/common/_config/Config.js
+++
@@ -1,6 +0,0 @@
-export const MapBoxConstants = {
-  MAPBOX_ACCESS_PLACES_API:
-    'https://api.mapbox.com/geocoding/v5/mapbox.places/',
-  MAPBOX_ACCESS_TOKEN:
-    'pk.eyJ1IjoiYmVuc2hleiIsImEiOiJjajFmZ2ludHMwMGx0MzJ0NDJzbW14MWc5In0.3W8kUIEbiliNAEl85DqD-A'
-};
